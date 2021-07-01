4bfc65bcd5a12aba76ccd2199127db8116caaad2
YounglanHong <yong2401@gmail.com>
2021-07-01 19:48:21 +0900

    🍱 Add icon titles to assets/menu

---
+++ assets/menu.js
@@ -0,0 +1,9 @@
+export const icons = [
+  { id: 0, icon: "My Computer" },
+  { id: 1, icon: "My Briefcase" },
+  { id: 2, icon: "My Documents" },
+  { id: 3, icon: "Recycle Bin" },
+  { id: 4, icon: "ToDo List" },
+  { id: 5, icon: "Text Editor" },
+  { id: 6, icon: "Paint" },
+] 
